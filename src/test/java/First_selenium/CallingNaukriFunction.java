b0a8ddb1de7ba11756ce60d51eba93af8b12cdbf
AnuragVighe <anuragvighe28498@gmail.com>
2025-01-24 12:34:10 +0530

    commit

--- src/test/java/First_selenium/CallingNaukriFunction.java
+++ src/test/java/First_selenium/CallingNaukriFunction.java
@@ -1,10 +1,13 @@
 package First_selenium;
 
+import org.testng.annotations.Test;
+
 public class CallingNaukriFunction extends updateNaukri{
     public CallingNaukriFunction() throws InterruptedException {
     }
 
-    public static void main(String[] args) throws InterruptedException {
+    @Test
+    public void CallingNaukriFunction() throws InterruptedException {
         updateNaukri UN=new updateNaukri();
         UN.updateNaukri("anuragvighe28498@gmail.com","Anurag@123");
         UN.updateNaukri("anuragvighe333@gmail.com","Anurag@123");
