a882093e36ade1b1cd8babcfcbc005791a495461
AnuragVighe <anuragvighe28498@gmail.com>
2025-01-25 11:06:19 +0530

    commit

--- src/test/java/First_selenium/CallingNaukriFunction.java
+++ src/test/java/First_selenium/CallingNaukriFunction.java
@@ -3,14 +3,17 @@ package First_selenium;
 import org.testng.annotations.Test;
 
 public class CallingNaukriFunction extends updateNaukri{
+    updateNaukri UN=new updateNaukri();
     public CallingNaukriFunction() throws InterruptedException {
     }
 
-    @Test
-    public void CallingNaukriFunction() throws InterruptedException {
-        updateNaukri UN=new updateNaukri();
-        UN.updateNaukri("anuragvighe28498@gmail.com","Anurag@123");
-        UN.updateNaukri("anuragvighe333@gmail.com","Anurag@123");
+    @Test(priority = 1)
+    public void CallingNaukriFunction28498() throws InterruptedException {
+        UN.updateNaukri("anuragvighe28498@gmail.com","Anurag@123","ANURAG28498");
+    }
+    @Test(priority = 2)
+    public void CallingNaukriFunction333() throws InterruptedException {
+        UN.updateNaukri("anuragvighe333@gmail.com","Anurag@123","ANURAG333");
     }
 }
 
--- src/test/java/First_selenium/updateNaukri.java
+++ src/test/java/First_selenium/updateNaukri.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class updateNaukri {
-    public void updateNaukri(String ID, String Pass) throws InterruptedException {
+    public void updateNaukri(String ID, String Pass,String msg) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver","C:\\Users\\HP\\Desktop\\mvn project\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
         WebDriver driver= new ChromeDriver();
         driver.get("https://www.naukri.com/mnjuser/profile?id=&altresid");
@@ -20,7 +20,7 @@ public class updateNaukri {
         driver.findElement(By.xpath("//*[contains(@type, 'submit') and text()='Login']")).click();
         //driver.findElement(By.className("nI-gNb-icon-img")).sendKeys(Keys.ENTER);
         Thread.sleep(4000);
-        driver.findElement(By.xpath("/html/body/main/div/div/div[3]/div/div[3]/div[2]/a")).click();
+        driver.findElement(By.xpath("//div[@class='view-profile-wrapper']//a[@href='/mnjuser/profile' and text()='Complete']")).click();
         Thread.sleep(2000);
         driver.findElement(By.xpath("//div[contains(@class, 'hdn')]//*[contains(@class, 'icon edit') and text()='editOneTheme']")).click();
         Thread.sleep(1000);
@@ -33,7 +33,7 @@ public class updateNaukri {
         driver.findElement(By.id("saveBasicDetailsBtn")).click();
         Thread.sleep(1000);
         System.out.println("-------------------------------------------------------------------------------------------");
-        System.out.println(ID+"- U P D A T E D _________ S U C C E S S F U L L Y");
+        System.out.println(msg+"- U P D A T E D _________ S U C C E S S F U L L Y");
         System.out.println("-------------------------------------------------------------------------------------------");
 
         driver.quit();
