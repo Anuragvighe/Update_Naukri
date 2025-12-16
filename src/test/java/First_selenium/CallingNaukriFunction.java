c6b6ba3ee6d9e9f090cbc8b5b8a0b4b774a1120b
AnuragVighe <anuragvighe28498@gmail.com>
2025-12-16 20:02:38 +0530

    all

--- src/test/java/First_selenium/CallingNaukriFunction.java
+++ src/test/java/First_selenium/CallingNaukriFunction.java
@@ -8,15 +8,15 @@ public class CallingNaukriFunction extends updateNaukri{
     }
 
     @Test(priority = 1)
-    public void CallingNaukriFunction28498() throws InterruptedException {
+    public void CallingNaukriFunction_Anurag28498() throws InterruptedException {
         UN.updateNaukri("anuragvighe28498@gmail.com","Anurag@123","ANURAG28498","e");
     }
     @Test(priority = 2)
-    public void CallingNaukriFunction333() throws InterruptedException {
+    public void CallingNaukriFunction_Anurag333() throws InterruptedException {
         UN.updateNaukri("anuragvighe333@gmail.com","Anurag@123","ANURAG333","e");
     }
     @Test(priority = 3)
-    public void CallingNaukriFunction28() throws InterruptedException {
+    public void CallingNaukriFunction_Prajwal() throws InterruptedException {
         UN.updateNaukri("vigheprajwal28@gmail.com","Pass@1234","PRAJWAL28","e");
     }
     @Test(priority = 4)
@@ -25,21 +25,21 @@ public class CallingNaukriFunction extends updateNaukri{
     }
 
     @Test(priority = 5)
-    public void CallingNaukriFunctionSap() throws InterruptedException {
+    public void CallingNaukriFunction_Sap() throws InterruptedException {
         UN.updateNaukri("dsapekshi@gmail.com","Sapekshi@1234","SAPEKSHI","o");
     }
 
     @Test(priority = 6)
-    public void CallingNaukriFunctionSunny() throws InterruptedException {
+    public void CallingNaukriFunction_Sunny() throws InterruptedException {
         UN.updateNaukri("sunnytayade89@gmail.com","Pooja@8912","SUNNY","e");
     }
     @Test(priority = 7)
-    public void CallingNaukriFunctionPallavi() throws InterruptedException {
+    public void CallingNaukriFunction_Pallavi() throws InterruptedException {
         UN.updateNaukri("virkhadepallavi28@gmail.com","Sept@is1234","PALLAVI","e");
     }
 
     @Test(priority = 8)
-    public void CallingNaukriFunctionChandan() throws InterruptedException {
+    public void CallingNaukriFunction_Chandan() throws InterruptedException {
         UN.updateNaukri("cchakrabarty954@gmail.com","Google@12345","CHANDAN","y");
     }
 }
